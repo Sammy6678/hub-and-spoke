4b91176250d51c6c70a959f9d906057e6d797098
Apekshaa12 <apeksha@bipinkdevops247gmail.onmicrosoft.com>
2025-06-25 16:08:01 +0530

    new commit

--- modules/privateendpoint/main.tf
+++ modules/privateendpoint/main.tf
@@ -1,18 +1,18 @@
-#resource "azurerm_private_endpoint" "private-blk"{
-    #for_each = var.private_endpoint
-    #name = each.value.name
-    #location = each.value.location 
-    #resource_group_name = each.value.resource_group_name
-    #subnet_id = var.subnet_ids[each.value.subnet_name]
+resource "azurerm_private_endpoint" "private-blk"{
+    for_each = var.private_endpoint
+    name = each.value.name
+    location = each.value.location 
+    resource_group_name = each.value.resource_group_name
+    subnet_id = var.subnet_ids[each.value.subnet_name]
 
 
-    #private_service_connection {
-    #name = "${each.key}-private-connection"
-   # is_manual_connection = "false"
-   # private_connection_resource_id = var.service_id
-   # subresource_names = each.value.subresource_names
-   # }
-#}
+    private_service_connection {
+    name = "${each.key}-private-connection"
+   is_manual_connection = "false"
+   private_connection_resource_id = var.service_id
+   subresource_names = each.value.subresource_names
+   }
+}
 
 #resource "azurerm_private_dns_zone" "dns-blk" {
    # for_each = var.private_dns
